67964babf70d8aa1834eb7914113099487112439
RainChan <mzdluo123@foxmail.com>
2022-01-20 22:42:08 +0800

    首个版本

--- static/lib/admin.js
+++ static/lib/admin.js
@@ -1,56 +1,25 @@
 'use strict';
 
-define('admin/plugins/quickstart', ['settings', 'uploader'], function (settings, uploader) {
+define('admin/plugins/avatar-replace-admin', ['settings'], function (settings) {
 	var ACP = {};
 
 	ACP.init = function () {
-		setupUploader();
-		settings.load('quickstart', $('.quickstart-settings'), function () {
-			setupColorInputs();
+		settings.load('avatar-replace', $('.avatar-replace-settings'), function () {
+
 		});
 		$('#save').on('click', saveSettings);
 	};
 
 	function saveSettings() {
-		settings.save('quickstart', $('.quickstart-settings'), function () {
+		settings.save('avatar-replace', $('.avatar-replace-settings'), function () {
 			app.alert({
 				type: 'success',
-				alert_id: 'quickstart-saved',
-				title: 'Settings Saved',
+				alert_id: 'avatar-replace-saved',
+				title: '设置已保存',
 				message: 'Please reload your NodeBB to apply these settings',
 				clickfn: function () {
 					socket.emit('admin.reload');
 				},
-			});
-		});
-	}
-
-	function setupColorInputs() {
-		var colorInputs = $('[data-settings="colorpicker"]');
-		colorInputs.on('change', updateColors);
-		updateColors();
-	}
-
-	function updateColors() {
-		$('#preview').css({
-			color: $('#color').val(),
-			'background-color': $('#bgColor').val(),
-		});
-	}
-
-	function setupUploader() {
-		$('#content input[data-action="upload"]').each(function () {
-			var uploadBtn = $(this);
-			uploadBtn.on('click', function () {
-				uploader.show({
-					route: config.relative_path + '/api/admin/upload/file',
-					params: {
-						folder: 'quickstart',
-					},
-					accept: 'image/*',
-				}, function (image) {
-					$('#' + uploadBtn.attr('data-target')).val(image);
-				});
 			});
 		});
 	}
---
+++ static/templates/admin/plugins/avatar-replace-admin.tpl
@@ -0,0 +1,21 @@
+<form role="form" class="avatar-replace-settings">
+	<div class="row">
+		<div class="col-sm-2 col-xs-12 settings-header">通用</div>
+		<div class="col-sm-10 col-xs-12">
+			<p class="lead">
+				启用头像替换后，将会把所有未上传到本站的头像删除
+			</p>
+			<div class="checkbox">
+				<label for="replace-switch" class="mdl-switch mdl-js-switch mdl-js-ripple-effect">
+					<input type="checkbox" class="mdl-switch__input" id="replace-switch" name="replace-switch">
+					<span class="mdl-switch__label"><strong>是否启用头像替换</strong></span>
+				</label>
+			</div>
+		</div>
+	</div>
+	<br />
+</form>
+
+<button id="save" class="floating-button mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+	<i class="material-icons">save</i>
+</button>
